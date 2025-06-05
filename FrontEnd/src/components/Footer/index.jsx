43fb779ca45fcfad5f1410031193cbccc1706394
Maria Eduarda de Pinho Braga <mpbduda@gmail.com>
2025-06-05 15:36:57 -0300

    feat: HomePage

---
+++ FrontEnd/src/components/Footer/index.jsx
@@ -0,0 +1,44 @@
+import { Instagram, Linkedin } from "lucide-react";
+import "../../styles/Footer.css";
+import { useNavigate } from "react-router-dom";
+
+function Footer() {
+  const navigate = useNavigate();
+
+  function returnPage(urls) {
+    urls.map((url) => {
+      window.open(url, "_blank", "noopener,noreferrer");
+    });
+  }
+  return (
+    <div className="footer">
+      <div className="footer-icons">
+        <Instagram
+          onClick={() => {
+            returnPage(["https://instagram.com"]);
+          }}
+        />
+        <Linkedin />
+        <img
+          src="/discord-gray-icon-border.svg"
+          alt="Discord"
+          className="footer-discord-icon"
+          onClick={() => {
+            returnPage([
+              "https://www.linkedin.com/in/maria-eduarda-de-pinho-braga-558057219/",
+              "https://www.linkedin.com/in/arthur-ataide-ufv/",
+              "https://www.linkedin.com/in/tarik-salles-paiva-977087269/",
+            ]);
+          }}
+        />
+      </div>
+      <img className="footer-logo" src="/logo.png" alt="ByteScan" 
+      onClick={() => {
+            returnPage(["https://discord.com"]);
+          }}/>
+      <span className="cop">© 2025 ByteScan</span>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ FrontEnd/src/components/HomePage/HomeMangaList.jsx
@@ -0,0 +1,82 @@
+import "../../styles/HomeMangaList.css";
+import StarRating from "../StarRating";
+import { useNavigate } from "react-router-dom";
+
+function HomeMangaList({ title, mangas }) {
+  const navigate = useNavigate();
+
+  console.log("HomeMangaList props:", {
+    title,
+    mangas,
+  });
+
+  function returnMangaPage(id) {
+    navigate(`/Manga/${id}`);
+  }
+
+  function returnMangaCap(idManga, idCap) {
+    navigate(`/Manga/${idManga}/${idCap}`);
+  }
+  return (
+    <div className="home-manga-list">
+      <div className="home-manga-list-container">
+        <h2 className="home-manga-list-title">{title}</h2>
+        <div className="home-manga-list-items">
+          {Array.isArray(mangas) &&
+            mangas.map((manga, idx) =>
+              manga && manga.imagemCapa && manga.nome && manga.id ? (
+                // Quando usa map tem de ter uma chave única
+                <div className="home-manga-card" key={idx}>
+                  <img
+                    src={manga.imagemCapa}
+                    alt={manga.nome}
+                    className="home-manga-card-img"
+                    onClick={() => {
+                      returnMangaPage(manga.id);
+                    }}
+                  />
+                  <div className="home-manga-card-info">
+                    <div
+                      className="home-manga-card-title"
+                      onClick={() => {
+                        returnMangaPage(manga.id);
+                      }}
+                    >
+                      {manga.nome}
+                    </div>
+                    <div className="home-manga-card-rating">
+                      <StarRating value={manga.avaliacao} />
+                    </div>
+                    {manga.lancamento && manga.lancamento.length > 0 && (
+                      <div className="home-manga-card-release">
+                        {manga.lancamento.map((lancamento, lidx) => (
+                          <div
+                            key={lidx}
+                            className="home-manga-card-release-item"
+                          >
+                            <button
+                              className="release-chapter"
+                              onClick={() => {
+                                returnMangaCap(manga.id, lancamento.idCap);
+                              }}
+                            >
+                              {lancamento.capitulo}
+                            </button>
+                            <span className="release-date">
+                              {lancamento.data}
+                            </span>
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              ) : null
+            )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomeMangaList;

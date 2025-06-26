5f39fc273690e29604b18819986c10b60384cfd4
Arthur-Ataide <arthursaraiva0704@gmail.com>
2025-06-26 19:36:31 -0300

    Update app.py

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -2,29 +2,31 @@ from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
+from database.db import db 
 
+# Carrega variáveis de ambiente
 load_dotenv()
 
 # Importa os blueprints
 from auth import auth_bp
 
+# Dicionário com as blueprints principais e seus prefixos
 blueprints = {
-        auth_bp: "/api/auth",
-    }
+    auth_bp: "/api/auth",
+}
 
 def create_app():
     app = Flask(__name__)
+    
+    # Configurações principais
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
+    db.init_app(app)
     CORS(app)
-    # db.init_app(app)  # se usar banco
 
-    # Dicionário com blueprint e prefixo
-    
-
-    # Registra todas as blueprints do dicionário
+    # Registra todas as blueprints
     for bp, prefix in blueprints.items():
         app.register_blueprint(bp, url_prefix=prefix)
 

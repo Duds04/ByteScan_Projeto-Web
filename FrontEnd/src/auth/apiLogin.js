be2f55fd10056c896c3c6dcf5a6da16eed4981a6
Arthur-Ataide <arthursaraiva0704@gmail.com>
2025-06-29 17:09:12 -0300

    Update apiLogin.js

--- FrontEnd/src/auth/apiLogin.js
+++ FrontEnd/src/auth/apiLogin.js
@@ -24,27 +24,6 @@ export async function loginAPI({ email, senha }) {
   };
 }
 
-// Função para registro
-export const loginAPI = async ({ email, senha }) => {
-  const response = await fetch("http://localhost:5000/api/auth/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email: email,
-      password: senha,
-    }),
-  });
-
-  if (!response.ok) {
-    const errorData = await response.json();
-    throw new Error(errorData.message || "Erro ao fazer login");
-  }
-
-  return await response.json();
-};
-
 export async function registerAPI({ nome, nomeUsuario, email, senha }) {
   const response = await fetch(`${BASE_URL}/cadastro`, {
     method: 'POST',

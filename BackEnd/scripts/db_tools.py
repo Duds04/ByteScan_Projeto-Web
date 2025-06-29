98dc892f4c43af7adf8bcd6e64c2c3cbc09a48c7
Arthur-Ataide <arthursaraiva0704@gmail.com>
2025-06-29 16:19:26 -0300

    banco de dados

--- BackEnd/scripts/db_tools.py
+++ BackEnd/scripts/db_tools.py
@@ -1,11 +1,11 @@
 import os
 import pymysql
 import sys
+from database.models import User, Obra, Capitulo, Avaliacao  # ajuste para o nome real do seu models.py
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app import create_app
 from database.db import db
-from database.models import User, Obra, Capitulo, Avaliacao  # ajuste para o nome real do seu models.py
 from scripts.db_mangas import create_obras
 import click
 

dd0d2e25db0a999dc71d136462bd07309af7a502
TarikSalles <tarik.paiva@ufv.br>
2025-06-29 19:20:39 -0300

    commit

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
           <Route
             path="/manga/:id/cap/:idCap"
             element={
-              // <PrivateRoute>
+              <PrivateRoute>
                 <ChapterPage />
               </PrivateRoute>
             }
